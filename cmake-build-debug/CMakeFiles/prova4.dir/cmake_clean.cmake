92c7cec0966a4e0e92c2fccaee820e6968c18c7f
leoke <leoviti9@gmail.com>
2023-07-20 17:37:33 +0200

    initial commit

---
+++ cmake-build-debug/CMakeFiles/prova4.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prova4.dir/main.cpp.o"
+  "CMakeFiles/prova4.dir/main.cpp.o.d"
+  "prova4"
+  "prova4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prova4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
